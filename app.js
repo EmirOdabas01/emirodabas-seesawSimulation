6e434fdc3dd8e57b1edaf26efb6625318f0cd0f8
Emir Odabas <odabasemir01@gmail.com>
2025-11-27 17:57:25 +0300

    object hover added

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
-let nextWeightComponent = document.querySelectorAll(".info-value")[1];
+const elements = {
+  infoNext: document.querySelectorAll(".info-value")[1],
+  objectPreview: document.getElementById("object-preview"),
+  plank: document.getElementById("seesaw-plank"),
+};
 
-let nextWeight = 0;
+let appState = {
+  nextWeight: 0,
+};
 
-getRandomWeight();
-updateInfo();
-
-function getRandomWeight() {
-  nextWeight = Math.floor(Math.random() * 10) + 1;
+function init() {
+  generateNextWeight();
+  updateUI();
 }
 
-function updateInfo() {
-  nextWeightComponent.innerText = nextWeight;
+function generateNextWeight() {
+  appState.nextWeight = Math.floor(Math.random() * 10) + 1;
 }
+
+function updateUI() {
+  elements.infoNext.innerText = appState.nextWeight;
+}
+
+elements.plank.addEventListener("mousemove", (event) => {
+  const rect = elements.plank.getBoundingClientRect();
+  const x = event.clientX - rect.left;
+  const constrainedX = Math.max(0, Math.min(x, rect.width));
+  elements.objectPreview.style.display = "flex";
+  elements.objectPreview.style.left = constrainedX - 15 + "px";
+  elements.objectPreview.style.top = "45%";
+  elements.objectPreview.innerText = appState.nextWeight;
+});
+
+elements.plank.addEventListener("mouseleave", () => {
+  elements.objectPreview.style.display = "none";
+});
+
+init();
